2178a3fd5c38d27638ec0dc50f161b30a22d7537
Muhammad Haris <m.haris425@gmail.com>
2026-05-29 15:16:31 -0400

    fix package name

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	userPort := flag.Int("port", 8080, "Port to connect server to: ")
+	flag.Parse()
+
+	address := fmt.Sprintf("localhost:%d", *userPort)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Error creating the Listener")
+	}
+
+	defer listener.Close() // close the listener when program exits main()
+
+	fmt.Printf("Server successfully listening on: %s", address)
+
+	for {
+		conn, err = listener.Accept()
+		if err != nil {
+			fmt.Println("Error in accepting Connection from Client.")
+			panic(err)
+		}
+		go confirmReception(conn)
+	}
+}
