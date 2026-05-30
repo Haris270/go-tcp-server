7f8a5a4e2724ad4d9553fcd96766011881596bf2
Muhammad Haris <m.haris425@gmail.com>
2026-05-29 22:18:38 -0400

    add infinite loop to keep connection open

--- cmd/server/confirmReception.go
+++ cmd/server/confirmReception.go
@@ -11,20 +11,22 @@ func confirmReception(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-	clientMessage, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error encountered during reading input")
-		return
-	}
+	for {
+		clientMessage, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error encountered during reading input")
+			return
+		}
+		fmt.Printf("Client: %s", clientMessage)
+		clientMessage = strings.TrimSpace(clientMessage)
 
-	clientMessage = strings.TrimSpace(clientMessage)
+		confirmationMsg := fmt.Sprintf("Confirmed: You sent %s\n", clientMessage)
 
-	confirmationMsg := fmt.Sprintf("Confirmed: You sent %s\n", clientMessage)
-
-	_, err = conn.Write([]byte(confirmationMsg)) //cast the response string to []byte
-	if err != nil {
-		fmt.Println("Error encountered while Writing output")
-		return
+		_, err = conn.Write([]byte(confirmationMsg)) //cast the response string to []byte
+		if err != nil {
+			fmt.Println("Error encountered while Writing output")
+			return
+		}
 	}
 
 }
