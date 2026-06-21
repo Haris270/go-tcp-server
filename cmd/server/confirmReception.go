2a42dd489984ad6f1a65271d6279f55bf0f94925
Muhammad Haris <m.haris425@gmail.com>
2026-06-21 09:49:58 -0400

    add code for adding/deleting clients to map and printing all clients

--- cmd/server/confirmReception.go
+++ cmd/server/confirmReception.go
@@ -7,24 +7,39 @@ import (
 	"strings"
 )
 
-func confirmReception(conn net.Conn) {
+func confirmReception(conn net.Conn, connMap map[string]net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
 	for {
 		clientMessage, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Error encountered during reading input")
+			//fmt.Println("Error encountered during reading input")
+			fmt.Println("Client Disconnected")
+			delete(connMap, conn.RemoteAddr().String())
 			return
 		}
 		fmt.Printf("Client: %s\n", clientMessage)
 		clientMessage = strings.TrimSpace(clientMessage)
+		var confirmationMsg string
+		if strings.EqualFold(clientMessage, "all clients") {
+			var allKeys strings.Builder
 
-		confirmationMsg := fmt.Sprintf("Confirmed: You sent %s\n", clientMessage)
+			for key := range connMap {
+				allKeys.WriteString(key)
+			}
+
+			result := allKeys.String()
+			confirmationMsg = fmt.Sprintf("Result: %s\n", result)
+		} else {
+			confirmationMsg = fmt.Sprintf("COnfirmed, you sent %s\n", clientMessage)
+		}
 
 		_, err = conn.Write([]byte(confirmationMsg)) //cast the response string to []byte
 		if err != nil {
-			fmt.Println("Error encountered while Writing output")
+			//fmt.Println("Error encountered while Writing output")
+			fmt.Println("Client Disconnected")
+			delete(connMap, conn.RemoteAddr().String())
 			return
 		}
 	}
