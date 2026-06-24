20b62a78ccfad694170681ae3ded55f923af71f8
Muhammad Haris <m.haris425@gmail.com>
2026-06-24 10:20:14 -0400

    refactor: added client initiation setup & extract network handling logic to dedicated helper funcs

--- cmd/server/confirmReception.go
+++ cmd/server/confirmReception.go
@@ -1,47 +1,67 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	"net"
 	"strings"
 )
 
-func confirmReception(conn net.Conn, connMap map[string]net.Conn) {
+func confirmReception(conn net.Conn, connMap map[string]Client) {
 	defer conn.Close()
 
-	reader := bufio.NewReader(conn)
+	//---------- initiating the connection -------------\\
+
+	val, ok := connMap[conn.RemoteAddr().String()]
+	var welcome_msg string
+	if ok {
+		var name string = val.Name
+		welcome_msg = fmt.Sprintf("Server: Welcome Back %s\n!", name)
+	} else {
+
+		welcome_msg = "Welcome! Enter Your Name: \n"
+	}
+	_, err := conn.Write([]byte(welcome_msg))
+	if err != nil {
+		fmt.Println("Error while forming connection")
+		return
+	}
+
+	clientName, err := msg_receiver(conn)
+	if err != nil {
+		fmt.Println("Error getting the name. Disconnecting...")
+		return
+	}
+	clientName = strings.TrimSuffix(clientName, "\n")
+	connMap[conn.RemoteAddr().String()] = Client{conn, clientName}
+
 	for {
-		clientMessage, err := reader.ReadString('\n')
+
+		clientMessage, err := msg_receiver(conn)
 		if err != nil {
-			//fmt.Println("Error encountered during reading input")
 			fmt.Println("Client Disconnected")
 			delete(connMap, conn.RemoteAddr().String())
 			return
 		}
-		fmt.Printf("Client: %s\n", clientMessage)
+
 		clientMessage = strings.TrimSpace(clientMessage)
-		var confirmationMsg string
-		if strings.EqualFold(clientMessage, "all clients") {
-			var allKeys strings.Builder
+		display_msg := fmt.Sprintf("%s: %s\n", clientName, clientMessage) // ClientName + ClientMessage to be displayed on Terminal
 
-			for key := range connMap {
-				allKeys.WriteString(key)
-			}
-
-			result := allKeys.String()
-			confirmationMsg = fmt.Sprintf("Result: %s\n", result)
-		} else {
-			confirmationMsg = fmt.Sprintf("COnfirmed, you sent %s\n", clientMessage)
-		}
-
-		_, err = conn.Write([]byte(confirmationMsg)) //cast the response string to []byte
-		if err != nil {
-			//fmt.Println("Error encountered while Writing output")
-			fmt.Println("Client Disconnected")
-			delete(connMap, conn.RemoteAddr().String())
+		// Broadcast the Message to all Clients (Including Sender)
+		broadcast_err := broadcast(connMap, display_msg)
+		if broadcast_err != nil {
+			fmt.Printf("Client Disconnected at Broadcast code\n")
 			return
 		}
+
 	}
 
 }
+
+/*
+	confirmReception() initiates the Client Connection by getting their name and adding them to the connMap. It
+	removes the newline delimiter from the name.
+
+	Within the for loop, the func calls the msg_receiver() and broadcast() funcs to receive client message and
+	broadcast the display_msg it to all clients (including the sender) respectively.
+*/
