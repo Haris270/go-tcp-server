d526e6df51b6f42dee4023736fadeabf933eda23
Muhammad Haris <m.haris425@gmail.com>
2026-07-28 23:42:51 -0400

    integrate struct funcs

--- cmd/server/confirmReception.go
+++ cmd/server/confirmReception.go
@@ -7,20 +7,20 @@ import (
 	"strings"
 )
 
-func confirmReception(conn net.Conn, connMap map[string]Client) {
+func confirmReception(conn net.Conn, connMap *ClientRegistry) { //map[string]Client) {
 	defer conn.Close()
 
 	//---------- initiating the connection -------------\\
 
-	val, ok := connMap[conn.RemoteAddr().String()]
-	var welcome_msg string
-	if ok {
-		var name string = val.Name
-		welcome_msg = fmt.Sprintf("Server: Welcome Back %s\n!", name)
-	} else {
+	// val, ok := connMap.getClient(conn.RemoteAddr().String())//connMap[conn.RemoteAddr().String()]
+	// var welcome_msg string
+	// if ok {
+	// 	var name string = val.Name
+	// 	welcome_msg = fmt.Sprintf("Server: Welcome Back %s\n!", name)
+	// } else {
 
-		welcome_msg = "Welcome! Enter Your Name: \n"
-	}
+	welcome_msg := "Welcome! Enter Your Name: \n"
+	// }
 	_, err := conn.Write([]byte(welcome_msg))
 	if err != nil {
 		fmt.Println("Error while forming connection")
@@ -33,14 +33,20 @@ func confirmReception(conn net.Conn, connMap map[string]Client) {
 		return
 	}
 	clientName = strings.TrimSuffix(clientName, "\n")
-	connMap[conn.RemoteAddr().String()] = Client{conn, clientName}
+	//connMap[conn.RemoteAddr().String()] = Client{conn, clientName}
+	connMap.addClient(conn, clientName)
+
+	join_notif := fmt.Sprintf("%s has joined the chat room!\n", clientName)
+	connMap.broadcast(join_notif)
 
 	for {
 
 		clientMessage, err := msg_receiver(conn)
 		if err != nil {
 			fmt.Println("Client Disconnected")
-			delete(connMap, conn.RemoteAddr().String())
+			//delete(connMap, conn.RemoteAddr().String())
+			connMap.removeClient(conn, conn.RemoteAddr().String())
+			connMap.broadcast(fmt.Sprintf("%s has left the chat\n", clientName))
 			return
 		}
 
@@ -48,10 +54,14 @@ func confirmReception(conn net.Conn, connMap map[string]Client) {
 		display_msg := fmt.Sprintf("%s: %s\n", clientName, clientMessage) // ClientName + ClientMessage to be displayed on Terminal
 
 		// Broadcast the Message to all Clients (Including Sender)
-		broadcast_err := broadcast(connMap, display_msg)
-		if broadcast_err != nil {
-			fmt.Printf("Client Disconnected at Broadcast code\n")
-			return
+		failed_clients := connMap.broadcast(display_msg)
+
+		if len(failed_clients) != 0 {
+
+			for _, client := range failed_clients {
+				connMap.removeClient(client.Connection, client.Name)
+			}
+
 		}
 
 	}
